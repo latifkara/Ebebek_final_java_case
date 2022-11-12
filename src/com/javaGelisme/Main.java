f3f5d23e853255860dcd0c51a3e4070cf3d09d2d
latifkara <latifkara93@gmail.com>
2022-11-12 18:47:16 +0300

    update Final project

--- src/com/javaGelisme/Main.java
+++ src/com/javaGelisme/Main.java
@@ -3,6 +3,6 @@ package com.javaGelisme;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/com/javaGelisme/Marka.java
@@ -0,0 +1,13 @@
+package com.javaGelisme;
+
+public class Marka {
+    public int id;
+    public String name;
+
+    public Marka(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
