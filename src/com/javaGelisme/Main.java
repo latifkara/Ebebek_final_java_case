a5f49a98b60a56bc4f4cbd89f3c7c8102e4cb27b
latifkara <latifkara93@gmail.com>
2022-11-13 11:03:47 +0300

    add function Final project

--- src/com/javaGelisme/Main.java
+++ src/com/javaGelisme/Main.java
@@ -3,6 +3,7 @@ package com.javaGelisme;
 public class Main {
 
     public static void main(String[] args) {
+        Marka marka = new Marka(1, "Samsung");
 
     }
 }
--- src/com/javaGelisme/Marka.java
+++ src/com/javaGelisme/Marka.java
@@ -9,5 +9,61 @@ public class Marka {
         this.name = name;
     }
 
+    public static class Samsung extends Marka {
+
+        public Samsung(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class Lenovo extends Marka {
+
+        public Lenovo(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class Apple extends Marka {
+
+        public Apple(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class Huawei extends Marka {
+
+        public Huawei(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class Casper extends Marka {
+
+        public Casper(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class Asus extends Marka {
+
+        public Asus(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class HP extends Marka {
+
+        public HP(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class Xiaomi extends Marka {
+
+        public Xiaomi(int id, String name) {
+            super(id, name);
+        }
+    }
+    public static class Monster extends Marka {
+
+        public Monster(int id, String name) {
+            super(id, name);
+        }
+    }
 
 }
+
+
